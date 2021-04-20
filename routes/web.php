0cc1aa963ba632edf7aefd4a4fa8d52919680954
Arman Arif <arman3472@aol.com>
2021-04-20 19:20:30 +0600

    router group

--- routes/web.php
+++ routes/web.php
@@ -2,34 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('blog.index');
-})->name('blog.index');
+    Route::get('/', function () {
+        return view('blog.index');
+    })->name('blog.index');
+    
+    Route::get('about', function () {
+        return view('other.about');
+    })->name('other.about');
+    
+    Route::get('post/{id}', function () {
+        return view('blog.post');
+    })->name('blog.post');
 
-Route::get('/about', function () {
-    return view('other.about');
-})->name('other.about');
 
-Route::get('/post/{id}', function () {
-    return view('blog.post');
-})->name('blog.post');
-
-Route::get('/admin', function () {
-    return view('admin.index');
-})->name('admin.index');
-
-Route::get('/admin/create', function () {
-    return view('admin.create');
-})->name('admin.create');
-
-Route::post('/admin/create', function () {
-    return "Save post";
-})->name('admin.create');
-
-Route::get('/admin/edit/{id}', function () {
-    return view('admin.edit');
-})->name('admin.edit');
-
-Route::post('/admin/edit', function () {
-    return "Post update";
-})->name('admin.update');
+Route::group(['prefix'=>'admin'],function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    })->name('admin.index');
+    
+    Route::get('create', function () {
+        return view('admin.create');
+    })->name('admin.create');
+    
+    Route::post('create', function () {
+        return "Save post";
+    })->name('admin.create');
+    
+    Route::get('edit/{id}', function () {
+        return view('admin.edit');
+    })->name('admin.edit');
+    
+    Route::post('edit', function () {
+        return "Post update";
+    })->name('admin.update');
+});
