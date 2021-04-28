b529b1c1da5d1df22d3523ac5fb3645c3558af0d
Arman Arif <arman3472@aol.com>
2021-04-28 09:40:45 +0600

    Post Model modified with method all, byId, add, edit;

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,44 @@ namespace App\Models;
 
 class Post
 {
+    public function all($session){
+        if(!$session->has('posts')){
+            $this->createDummyData($session);
+        }
+        return $session->get('posts');
+    }
 
+    public function byId($session,$id){
+        return $this->all($session)[$id];
+    }
+
+    public function add($session, $title, $content){
+        $posts = $this->all($session);
+        array_push($posts, ['title'=>$title,'content'=>$content]);
+        $session->put('posts',$posts);
+    }
+
+    public function edit($session, $id, $title, $content){
+        $posts = $this->all($session);
+        $posts[$id] = ['title' => $title, 'content' => $content];
+        $session->put('posts', $posts);
+    }
+
+    private function createDummyData($session){
+        $posts = [
+            [
+                'title' => 'Learning Laravel',
+                'content' => 'This blog post will get you right on track with Laravel!',
+            ],
+            [
+                'title' => 'The next Steps',
+                'content' => 'Understanding the Basics is great, but you need to be able to make the next steps.',
+            ],
+            [
+                'title' => 'Something else',
+                'content' => 'Some other content',
+            ]
+        ];
+        $session->put('posts', $posts);
+    }
 }
