9304aad41d3ea4300ccf21d8d1f164e14d6859c2
Arman Arif <me@armanarif.com>
2021-05-03 01:49:43 +0600

    dynamic page title added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@isset($title) $title . ' | ' @endisset{{ config('app.name') }}</title>
+    <title>@isset($title) {{ $title }} | @endisset {{ config('app.name') }}</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     {{-- <link href='http://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet' type='text/css'> --}}
