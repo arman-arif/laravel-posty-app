24755e81d46a6c6985bfa26fdf155af7e960e75c
Arman Arif <me@armanarif.com>
2021-05-08 00:33:50 +0600

    Post view modified with Tags

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -9,10 +9,17 @@
         <div class="col-md-12">
             <p>{{ count($post->likes) }} Likes |
                 <a href="{{ route('blog.post.like', $post->id) }}">Like</a></p>
-        </div>
 
-        <div class="col-md-12">
             <p>{{ $post->content }}</p>
+
+            @if (count($post->tags))
+                <p>
+                    Tags:
+                    @foreach($post->tags as $tag)
+                        <span class="badge badge-secondary">{{ $tag->name }}</span>
+                    @endforeach
+                </p>
+            @endif
         </div>
     </div>
 @endsection
