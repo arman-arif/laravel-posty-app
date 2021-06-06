feef58107b7b7640c32a7d5d421519b4d866d427
Arman Arif <arman3472rmn@gmail.com>
2021-06-06 23:58:34 +0600

    login view modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,14 +11,14 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="help-block" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
