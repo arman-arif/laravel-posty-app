facb5d79577422553e4061c74725a66c5d5ef798
arman-arif <arman3472rmn@gmail.com>
2021-06-12 08:19:39 +0600

    Header updated with username

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,7 +18,8 @@
                 </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        Username <span class="caret"></span>
+                        {{ Auth::check() ? Auth::user()->name : 'Username' }}
+                        <span class="caret"></span>
                     </a>
 
                     <ul class="dropdown-menu" role="menu">
