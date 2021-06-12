68785a626a1ad45db2716d90e66cba219d3a1edb
arman-arif <arman3472rmn@gmail.com>
2021-06-12 08:12:15 +0600

    Action for custom auth -> signin() method created

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SigninController extends Controller
 {
-    //
+    public function signin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => ['required','email'],
+            'password' => 'required|string'
+        ]);
+
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ], $request->has('remember'))) {
+            return redirect()->route('admin.index');
+        }
+
+        return redirect()->back()->with('error', 'Authentication failed!');
+    }
 }
